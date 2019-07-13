acbb4bd1c2284e5fdb0c58bd2e9b15dd4481f789
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-07-13 03:20:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @analyzed_text = PigLatinizer.new(params[:user_input])
+    PigLatinizer.new(params[:user_input])
 
     erb :results
   end
